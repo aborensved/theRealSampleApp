cf1b82ebce82992d411ef552f644b99889052949
aborensved <arvid.borensved@utb.ecutbildning.se>
2022-04-26 00:13:04 +0200

    Finish user edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,19 +4,55 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
+	def remember(user)
+		user.remember
+		cookies.permanent.encrypted[:user_id] = user.id
+		cookies.permanent[:remember_token] = user.remember_token
+	end
+
 	# Returns the current logged-in user (if any)
 	def current_user
-		if session[:user_id]
-			@current_user ||= User.find_by(id: session[:user_id])
+		if (user_id = session[:user_id])
+			@current_user ||= User.find_by(id: user_id)
+		elsif (user_id = cookies.encrypted[:user_id])
+			user = User.find_by(id: user_id)
+			if user && user.authenticated?(cookies[:remember_token])
+				log_in user
+				@current_user = user
+			end
 		end
+	end
+
+	#return true if the given user is the current user.
+	def current_user?(user)
+		user && user == current_user
 	end
 
 	def logged_in?
 		!current_user.nil?
 	end
 
+	# Forgets a persistent session.
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end	
+
 	def log_out
+		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
+	end
+
+	# Redirects to stored location (or to the default)
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	# Stores the URL trying to be accessed.
+	def store_location
+		session[:forwarding_url] = request.original_url if request.get?
 	end
 end
